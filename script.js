0beaadb5f418f5ba50d3e009754397a9b9fa4bd1
akanksha khan <41534307+aks21@users.noreply.github.com>
2024-01-30 10:55:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let extensionId = "niabohgmmladbgipommcolmbobckcjjk"; // Replace with your actual extension ID
 
 // Send a message to the extension
-chrome.runtime.sendMessage(extensionId, { message: "This is a sample test message from the web app" }, function(response) {
+chrome.runtime.sendMessage(extensionId, { message: "This is a sample test message from the web app from akanksha" }, function(response) {
   if (chrome.runtime.lastError) {
     console.error("Error sending message to extension:", chrome.runtime.lastError);
   } else {
